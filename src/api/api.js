ef68a61d65dba974386220b3d147a57639155a44
adarshKris <krishna.adarsh@careesteem.co.uk>
2026-02-26 13:58:33 +0000

    updayte

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// const BASE_URL = "http://localhost:5000/api";
 const BASE_URL = "https://broadband-backend.onrender.com/api";
 
 const api = axios.create({ baseURL: BASE_URL });
